686c1f2d638ce50f199760c0536cb90eab027221
Vincent <verillytmt@gmail.com>
2026-01-04 21:03:31 +1100

    Replaced axios API call with fetch

--- backend/ragRoutes.js
+++ backend/ragRoutes.js
@@ -1,20 +1,20 @@
 import express from 'express';
-import axios from 'axios';
 
 const router = express.Router();
-const RAG_URL = process.env.RAG_URL || 'http://rag:8000';
+const RAG_URL = 'http://rag:8000';
 
 //query
 router.post('/query', async (req, res) => {
   try {
     const { question } = req.body;
     if (!question) return res.status(400).json({ error: 'Question is required' });
-
-    const response = await axios.post(`${RAG_URL}/query`, {
-      params: { query: question },
+    const response = await fetch(`${RAG_URL}/query`, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({question: question})
     });
-
-    res.json(response.data);
+    const result = await response.json()
+    return res.json(result);
   } catch (err) {
     res.status(500).json({ error: 'RAG query failed', details: err.message });
   }

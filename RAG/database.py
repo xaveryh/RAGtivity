eca32a8b8c148cdda1c8999ae85454a34ddd0f70
ngohainnam <ngohainnam@gmail.com>
2026-01-09 20:46:45 +1100

    fix .env file problem, delete unused file (frontend and old document loader)

--- RAG/database.py
+++
@@ -1,8 +0,0 @@
-from config import embeddings
-from langchain_chroma import Chroma
-
-vector_store = Chroma(
-    collection_name="rag_collection",
-    embedding_function=embeddings,
-    persist_directory="./chroma_langchain_db",
-)
